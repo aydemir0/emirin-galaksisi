e7e3b39cd3f0d1a6f7da1a051cb19ae4515b00a7
aydemir0 <aydinmuhammedemir9@gmail.com>
2025-10-21 13:02:50 +0300

    Update ArkaPlan.jsx

--- src/components/ArkaPlan.jsx
+++ src/components/ArkaPlan.jsx
@@ -1,19 +1,19 @@
-// src/components/ArkaPlan.jsx
+
 "use client";
 
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import * as THREE from "three";
 
-// onClick prop'unu geri ekliyoruz
+
 const ArkaPlan = ({ onClick }) => { 
   const texture = useLoader(TextureLoader, "/textures/uzay_arkaplan.jpg");
 
   return (
     <mesh
       scale={[-1, 1, 1]}
-      onClick={onClick} // onClick'i mesh'e geri bağlıyoruz
-      // raycast={() => null} satırını sildik!
+      onClick={onClick} 
+      
     > 
       <sphereGeometry args={[500, 60, 40]} />
       <meshBasicMaterial map={texture} side={THREE.BackSide} />
