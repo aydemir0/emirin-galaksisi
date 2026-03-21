10c8d61d836557597d7d2ea0af7737c8444f1440
aydemir0 <aydinmuhammedemir9@gmail.com>
2026-03-21 03:40:36 +0300

    chore: Update Google Site Verification code for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
 
   // --- GOOGLE DOĞRULAMA KODUN BURADA ---
   verification: {
-    google: 'Fputwk0VeOiqU8y3DBm3fIT9SGEs45jqs_3OHAJ0aqY',
+    google: 'tnDxhFduUeVAFNQnjWrGZg4rAsS2RTWCsOtRskudI1o',
   },
 };
 
